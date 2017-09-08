a62b4aea54aa1076d6236fc6932779d76f733630
anjithadevan <anjithadevan@gmail.com>
2017-09-08 20:19:27 +0530

    changed resume name and path

--- mysite/urls.py
+++ mysite/urls.py
@@ -12,7 +12,7 @@ urlpatterns = [
     # Examples:
     # url(r'^$', 'mysite.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-	url(r'^myapp/', include('myapp.urls')),		
+	url(r'', include('myapp.urls')),		
     url(r'^admin/', include(admin.site.urls)),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
