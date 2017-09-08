ec2ad677a2e1bd6c77551cd3c86345d956277959
anjithadevan <anjithadevan@gmail.com>
2017-09-08 14:19:11 +0530

    First commit

---
+++ mysite/urls.py
@@ -0,0 +1,18 @@
+from django.conf.urls import include, url
+from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib.staticfiles import views
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
+admin.autodiscover()
+
+
+urlpatterns = [
+    # Examples:
+    # url(r'^$', 'mysite.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+	url(r'^myapp/', include('myapp.urls')),		
+    url(r'^admin/', include(admin.site.urls)),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
